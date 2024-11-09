372c1341a215e623865f2e8350177cf0a9dbabe6
Ulia Lukash <ulianalukash@yandex.ru>
2024-11-09 18:30:06 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-2xl">Hello, React with TypeScript, Redux, Webpack, and Tailwind CSS!</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    static: './dist',
+  }
+};
